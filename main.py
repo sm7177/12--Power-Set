a2e3a933b776e46f7f7a0a1fceb0b56b1582a015
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-28 18:54:06 +0545

    .

--- main.py
+++ main.py
@@ -26,10 +26,48 @@
 
 
 
-def single(nums):
-    result=0
-    for num in nums:
-        result^=num
-    return result
+# def single(nums):
+#     result=0
+#     for num in nums:
+#         result^=num
+#     return result
 
-print(single([1,2,3,2,1]))
+# print(single([1,2,3,2,1]))
+
+
+
+
+
+
+
+
+
+# def flip (num1,num2):
+#     flip=0
+#     while(num1>0 or num2>0):
+#         t1=num1 & 1
+#         t2=num2 & 1
+#         if t1!=t2:
+#             flip+=1
+#         num1>>=1
+#         num2>>=1
+#     return flip
+
+# num1=int(input("Enter num1:"))
+# num2=int(input("Enter num2:"))
+
+# print("Number of flips required:",flip(num1,num2))
+
+
+
+
+
+
+def add(a,b):
+    while b!=0:
+        carry=a & b
+        a=a ^ b
+        b=carry << 1
+    return a
+
+print(add(10, 5))
